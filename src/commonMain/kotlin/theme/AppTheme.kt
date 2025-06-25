0d81b3bf07a3acb21a04cfbc63f0cd4b1794433a
Hakanai <hakanai@ephemeral.garden>
2025-06-26 04:15:46 +1000

    Fix AppTheme not using passed-in option

--- src/commonMain/kotlin/theme/AppTheme.kt
+++ src/commonMain/kotlin/theme/AppTheme.kt
@@ -2,19 +2,19 @@ package garden.ephemeral.glyphplay.theme
 
 import androidx.compose.foundation.LocalScrollbarStyle
 import androidx.compose.foundation.defaultScrollbarStyle
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.graphics.Color
 
 
 @Composable
 fun AppTheme(option: ThemeOption = ThemeOption.SYSTEM_DEFAULT, content: @Composable () -> Unit) {
-    val colorScheme = darkColorScheme(
-        primary = Color(233, 55, 84),
-        secondary = Color(65, 152, 223),
-    )
+    val colorScheme = when (option) {
+        ThemeOption.LIGHT -> LightColors
+        ThemeOption.DARK -> DarkColors
+        ThemeOption.SYSTEM_DEFAULT -> if (isSystemInDarkTheme()) DarkColors else LightColors
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
